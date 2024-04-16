6b95b0f61f78e4a779f51d495fcfb48d2cdedbda
mguzelocak <mguzelocak@carroll.edu>
2024-04-16 15:39:24 -0600

    final footer and menu

--- assignments/menu/js/menu.js
+++ assignments/menu/js/menu.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector(".nav-menu");
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.querySelector('.menu_icon');
+  const navMenu = document.querySelector('.nav-menu');
 
-    menuToggle.addEventListener("click", function () {
-      navMenu.classList.toggle("active");
-    });
+  menuToggle.addEventListener('click', function() {
+      navMenu.classList.toggle('active');
+  });
 });
